677d1c1982f060fb69005a82f6bd637d3ddf5e78
c4thyc <cathyc22@nycstudents.net>
2025-05-01 05:33:56 +0000

    done

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -17,12 +17,25 @@ public class AppointmentBook {
         }
         return -1;
     }
-    public boolean makeAppointmentBook(int startPeriod, int endPeriod, int duration) {
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration) {
+        for (int i = startPeriod; i <= endPeriod; i++) {
+            int freeBlock = findFreeBlock(i, duration);
+            if (freeBlock > -1) {
+                reserveBlock(i, freeBlock, duration);
+                return true;
+            }
+        }
         return false;
     }
     public void printPeriod(int period) { 
         for(int i = 0; i < schedule[period - 1].length; i++) {
             System.out.println(i + " " + schedule[period - 1][i]);
+        }
+    }
+
+    private void reserveBlock(int period, int startMinute, int duration) {
+        for (int i = startMinute; i < startMinute + duration; i++) {
+            schedule[period - 1][i] = false;
         }
     }
  }
